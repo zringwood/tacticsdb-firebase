47dc6f45f942b5c4c44c14b44e5f15cc117f2258
Zachary Ringwood <zringwood@gmail.com>
2023-07-31 12:45:32 -0400

    Added routes for difficulty

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors = require('cors')
 const  corsOptions = { origin: process.env.FRONTEND_URL}
 app.use(cors(corsOptions))
 
+
+
 //Routes
 const endgames = require("./routes/endgames.js")
 const middlegames = require("./routes/middlegames.js")
