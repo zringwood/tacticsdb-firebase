6f75ba5204ee41bd7fd923e70194e42370a48cb0
Zachary Ringwood <zringwood@gmail.com>
2023-07-31 12:48:30 -0400

    Removed calls to console.

--- util/admin.js
+++ util/admin.js
@@ -2,7 +2,7 @@ const admin = require("firebase-admin");
 require('dotenv').config();
 
 const serviceAccount = require(`../${process.env.ADMIN_SERVICE_ACCOUNT}`);
-console.log(process.env.DATABASE_URL)
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.DATABASE_URL
