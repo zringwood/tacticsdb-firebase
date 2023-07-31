c5cde857ba1191c67b64c7610c0fb6e6e73a6c61
Zachary Ringwood <zringwood@gmail.com>
2023-07-31 11:00:17 -0400

    env implemented. keys.json can be removed.

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-let admin = require("firebase-admin");
+const admin = require("firebase-admin");
 require('dotenv').config();
 
-let serviceAccount = require(`./${process.env.ADMIN_SERVICE_ACCOUNT}`);
-
+const serviceAccount = require(`./${process.env.ADMIN_SERVICE_ACCOUNT}`);
+console.log(process.env.DATABASE_URL)
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: `${process.env.DATABASE_URL}`
+  databaseURL: process.env.DATABASE_URL
 });
 
 const db = admin.database()
--- util/admin.js
+++ util/admin.js
@@ -1,13 +1,13 @@
-let admin = require("firebase-admin");
+const admin = require("firebase-admin");
 require('dotenv').config();
 
-let serviceAccount = require("../tacticsdb-393323-firebase-adminsdk-uxmzf-e18f16d5ce.json");
-
+const serviceAccount = require(`../${process.env.ADMIN_SERVICE_ACCOUNT}`);
+console.log(process.env.DATABASE_URL)
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://tacticsdb-393323-default-rtdb.firebaseio.com'
+  databaseURL: process.env.DATABASE_URL
 });
 
-const db = admin.firestore()
+const db = admin.database()
 
 module.exports = { db}
