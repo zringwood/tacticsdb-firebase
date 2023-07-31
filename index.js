83949e08dbea269b595fcf634afebd0fb29739ad
Zachary Ringwood <zringwood@gmail.com>
2023-07-31 11:04:07 -0400

    refactoring

--- index.js
+++ index.js
@@ -1,28 +1,18 @@
-const express = require('express');
-const admin = require("firebase-admin");
-require('dotenv').config();
-
-const serviceAccount = require(`./${process.env.ADMIN_SERVICE_ACCOUNT}`);
-console.log(process.env.DATABASE_URL)
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.DATABASE_URL
-});
-
-const db = admin.database()
+const express = require('express')
+const {db }= require('./util/admin.js')
 
 const app = express();
 const port =  process.env.PORT || 5050;
 const cors = require('cors')
-const  corsOptions = { origin: 'https://tactics.zacharyringwood.com'}
+const  corsOptions = { origin: process.env.FRONTEND_URL}
 app.use(cors(corsOptions))
-// Define your routes and APIs here
+
 //Serves an endgame puzzle with a given id
 app.get("/middlegames/easy/:id", async (req, res) => {
     const puzzleId = req.params.id;
 
   try {
-    const ref = admin.database().ref('endgames-easy');
+    const ref = db.ref('endgames-easy');
     const snapshot = await ref.child(puzzleId).once("value");
 
     return res.status(200).send(snapshot);
