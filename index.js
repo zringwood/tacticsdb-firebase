5dc0639cdd321f57c6d59519594a6f1d501205e2
Zachary Ringwood <zringwood@gmail.com>
2023-07-31 10:54:33 -0400

    environment variables

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 let admin = require("firebase-admin");
 require('dotenv').config();
 
-let serviceAccount = require("./tacticsdb-393323-firebase-adminsdk-uxmzf-e18f16d5ce.json");
+let serviceAccount = require(`./${process.env.ADMIN_SERVICE_ACCOUNT}`);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://tacticsdb-393323.firebaseio.com/'
+  databaseURL: `${process.env.DATABASE_URL}`
 });
 
 const db = admin.database()
